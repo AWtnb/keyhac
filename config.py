eac70a6fbf226989d674c68cb203581a00843242
awtnb <arikaebanataw@gmail.com>
2024-01-26 09:47:37 +0900

    update

--- config.py
+++ config.py
@@ -991,6 +991,7 @@ def configure(keymap):
     SKK_TO_KANAMODE.apply_pair(
         keymap_global,
         {
+            "U1-AtMark": [" `", "` "],
             "U0-8": ["\u300e", "\u300f"],  # WHITE CORNER BRACKET 『』
             "U0-9": ["\u3010", "\u3011"],  # BLACK LENTICULAR BRACKET 【】
             "U0-OpenBracket": ["\u300c", "\u300d"],  # CORNER BRACKET 「」
@@ -1022,7 +1023,6 @@ def configure(keymap):
             "U0-2": ['"', '"'],
             "U0-7": ["'", "'"],
             "U0-AtMark": ["`", "`"],
-            "U1-AtMark": [" `", "` "],
             "U0-CloseBracket": ["[", "]"],
             "U1-9": ["(", ")"],
             "U1-CloseBracket": ["{", "}"],
