0844cdf5d482819323f65bf20acb238933454c09
awtnb <arikaebanataw@gmail.com>
2025-09-03 09:16:35 +0900

    reduce unused

--- Keyhac/config.py
+++ Keyhac/config.py
@@ -1037,39 +1037,14 @@ def configure(keymap):
                 sequence = circumfix + ["Left"] * len(suffix)
                 km[key] = self.invoke(*sequence)
 
-    keymap_global["U0-M"] = keymap.defineMultiStrokeKeymap("combo start. repeat by:")
+    keymap_global["U0-M"] = keymap.defineMultiStrokeKeymap()
 
-    def cursor_combo(km: WindowKeymap) -> None:
-        sender = SKKSender()
-        mod = "LS-"
-        number = "23456789"
-        base_combo = {
-            "H": "Left",
-            "J": "Down",
-            "K": "Up",
-            "L": "Right",
-        }
-        delete_combo = {
-            "B": "Back",
-            "D": "Delete",
-        }
-        for n in number:
-            km[n] = keymap.defineMultiStrokeKeymap("repeat {} times:".format(n))
-            for key, to in (base_combo | delete_combo).items():
-                seq = [to] * int(n)
-                km[n][key] = seq
-                if key in base_combo:
-                    sel_seq = [mod + s for s in seq]
-                    km[n][mod + key] = sel_seq
-
-    cursor_combo(keymap_global["U0-M"])
-
-    def honorific_last_nchar(km: WindowKeymap, honorific: str) -> Callable:
+    def replace_last_nchar(km: WindowKeymap, newstr: str) -> Callable:
         for n in "123":
-            seq = ["Back"] * int(n) + [honorific]
-            km["LC-" + n] = LatinSender().invoke(*seq)
+            seq = ["Back"] * int(n) + [newstr]
+            km[n] = LatinSender().invoke(*seq)
 
-    honorific_last_nchar(keymap_global["U0-M"], "先生")
+    replace_last_nchar(keymap_global["U0-M"], "先生")
 
     # markdown list
     keymap_global["S-U0-8"] = LatinSender().invoke("- ")
