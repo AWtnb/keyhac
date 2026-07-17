5f5d91fc49d9dec450a4b5fa0254a56b64bc8fd0
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-07-17 14:05:01 +0900

    change keybind

--- Keyhac/config.py
+++ Keyhac/config.py
@@ -2178,7 +2178,7 @@ def configure(keymap) -> None:
     keymap_smoothcsv["S-Space"] = DirectSender().invoke("S-Space")
     keymap_smoothcsv["S-U0-N"] = lambda: VirtualFinger(20).send("F2", "Home")
 
-    keymap_smoothcsv["A-Q"] = DirectSender(20).invoke(" LIKE '%%'", "Left", "Left")
+    keymap_smoothcsv["F9"] = DirectSender(20).invoke(" LIKE ", "'%%'", "Left", "Left")
 
     def copy_and_unselect_line() -> None:
         finger = VirtualFinger()
