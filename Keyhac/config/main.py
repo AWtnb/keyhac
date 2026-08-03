7beda957dab87f4163516d0dda23e45cb232a288
awtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-03 22:59:02 +0900

    split clipboard formatter

--- Keyhac/config/main.py
+++ Keyhac/config/main.py
@@ -1,9 +1,6 @@
 import os
-import re
 import subprocess
-import urllib.parse
 import webbrowser
-from collections.abc import Callable
 
 import ckit  # type: ignore
 import pyauto  # type: ignore
@@ -21,12 +18,8 @@ from .tools import window_activate as window_activate_tool
 from .tools import window_snap as window_snap_tool
 from .tools.browser_info import SystemBrowser
 from .tools.char_width import (
-    to_full_brackets,
     to_full_letter,
-    to_full_symbol,
-    to_half_brackets,
     to_half_letter,
-    to_half_symbol,
 )
 from .tools.clipboard import copy_then, invoke_clean_paster, paste
 from .tools.common import (
@@ -41,13 +34,11 @@ from .tools.common import (
     shell_exec,
     smart_check_path,
 )
+from .tools.format_clipboard import CLIPBOARD_FORMATTER_MAPPING
 from .tools.format_str import (
-    as_single_line,
-    remove_whitespace,
+    as_single_quoted_line,
     simple_quote,
-    skip_blank_line,
 )
-from .tools.punctuation import KANGXI_RADICAL_MAPPING, RADICAL_MAPPING
 from .tools.web_search import invoke_web_seacher
 from .tools.window_activate import WindowActivator, WndScanner
 from .tools.window_rect import RectEdge
@@ -233,8 +224,13 @@ def setup(keymap) -> None:
 
     # paste with quote mark
     keymap_global["U1-Q"] = lambda: paste(None, simple_quote)
-    keymap_global["LC-U1-Q"] = lambda: paste(None, as_single_line)
-    keymap_global["LS-U1-Q"] = lambda: paste(None, skip_blank_line)
+    keymap_global["LC-U1-Q"] = lambda: paste(None, as_single_quoted_line)
+
+    # paste as fullwidth / halfwidth
+    keymap_global["U1-W"] = lambda: paste(format_func=lambda s: to_full_letter(s, True))
+    keymap_global["LS-U1-W"] = lambda: paste(
+        format_func=lambda s: to_half_letter(s, True)
+    )
 
     # open url in browser
     def open_selected_url() -> None:
@@ -749,318 +745,6 @@ def setup(keymap) -> None:
     # popup clipboard menu
     ################################
 
-    keymap_global["U1-W"] = lambda: paste(format_func=lambda s: to_full_letter(s, True))
-    keymap_global["LS-U1-W"] = lambda: paste(
-        format_func=lambda s: to_half_letter(s, True)
-    )
-
-    class NestedCircumfix:
-        def __init__(self, prime_pair: tuple, secondary_pair: tuple):
-            self.pairs = [prime_pair, secondary_pair]
-
-        def fix(self, s: str) -> str:
-            stack = []
-            result = list(s)
-            openChar, closeChar = self.pairs[0]
-            for i, char in enumerate(s):
-                if char == openChar:
-                    stack.append(i)
-                elif char == closeChar:
-                    if stack:
-                        start = stack.pop()
-                        depth = len(stack)
-                        left, right = self.pairs[depth % 2]
-                        result[start] = left
-                        result[i] = right
-
-            return "".join(result)
-
-    class FormatTools:
-        @staticmethod
-        def to_deepl_friendly(s: str) -> str:
-            ss = []
-            lines = s.splitlines()
-            for line in lines:
-                if line.endswith(" "):
-                    ss.append(line)
-                elif line.endswith("-"):
-                    ss.append(line[0:-1])
-                else:
-                    ss.append(line + " ")
-            return "".join(ss).strip()
-
-        @staticmethod
-        def swap_abbreviation(s: str) -> str:
-            ss = re.split(r"[:：]\s*", s)
-            if len(ss) == 2:
-                return ss[1] + "：" + ss[0]
-            return ""
-
-        @staticmethod
-        def colon_to_doubledash(s: str) -> str:
-            return re.sub(r"[:：]\s*", "\u2015\u2015", s)
-
-        @staticmethod
-        def skip_blank_line(s: str) -> str:
-            lines = s.strip().splitlines()
-            return "\n".join([line for line in lines if line.strip()])
-
-        @staticmethod
-        def insert_blank_line(s: str) -> str:
-            lines = []
-            for line in s.strip().splitlines():
-                lines.append(line.strip())
-                lines.append("")
-            return "\n".join(lines)
-
-        @staticmethod
-        def to_double_bracket(s: str) -> str:
-            reg = re.compile(r"[\u300c\u300d]")
-
-            def _replacer(mo: re.Match) -> str:
-                if mo.group(0) == "\u300c":
-                    return "\u300e"
-                return "\u300f"
-
-            return reg.sub(_replacer, s)
-
-        @staticmethod
-        def to_single_bracket(s: str) -> str:
-            reg = re.compile(r"[\u300e\u300f]")
-
-            def _replacer(mo: re.Match) -> str:
-                if mo.group(0) == "\u300e":
-                    return "\u300c"
-                return "\u300d"
-
-            return reg.sub(_replacer, s)
-
-        @staticmethod
-        def to_list(s: str) -> str:
-            lines = s.splitlines()
-            return "\n".join(["- " + line for line in lines])
-
-        @staticmethod
-        def split_postalcode(s: str) -> str:
-            lines = s.splitlines()
-            if 1 < len(lines):
-                reg = re.compile(r"(\d{3}).(\d{4})[ 　]*(.+$)")
-            else:
-                reg = re.compile(r"(\d{3}).(\d{4})[\s]*(.+$)")
-            ss = []
-            for line in lines:
-                hankaku = to_half_letter(line.strip().strip("\u3012"), True)
-                m = reg.match(hankaku)
-                if m:
-                    ss.append(f"{m.group(1)}-{m.group(2)}\t{m.group(3)}")
-                else:
-                    ss.append(line)
-            return "\n".join(ss)
-
-        @staticmethod
-        def fix_paren_inside_bracket(s: str) -> str:
-            reg = re.compile(r"(\(.+?\)|（.+?）)」")
-
-            def _replacer(mo: re.Match) -> str:
-                return "」" + mo.group(1)
-
-            return reg.sub(_replacer, s)
-
-        @staticmethod
-        def fix_dumb_quotation(s: str) -> str:
-            reg = re.compile(r"\"([^\"]+?)\"|'([^']+?)'")
-
-            def _replacer(mo: re.Match) -> str:
-                if str(mo.group(0)).startswith('"'):
-                    return f"\u201c{mo.group(1)}\u201d"
-                return f"\u2018{mo.group(1)}\u2019"
-
-            return reg.sub(_replacer, s)
-
-        @staticmethod
-        def decode_url(s: str) -> str:
-            return urllib.parse.unquote(s)
-
-        @staticmethod
-        def encode_url(s: str) -> str:
-            return urllib.parse.quote(s)
-
-        @staticmethod
-        def trim_honorific(s: str) -> str:
-            reg = re.compile(r"先生$|様$|(先生|様)(?=[、。：；（）［］・！？\s])")
-            return reg.sub("", s)
-
-        @staticmethod
-        def trim_space_on_line_head(s: str) -> str:
-            return "\n".join([line.lstrip() for line in s.splitlines()])
-
-        @staticmethod
-        def format_nested_paren(s: str) -> str:
-            return NestedCircumfix(("（", "）"), ("〔", "〕")).fix(s)
-
-        @staticmethod
-        def format_nested_bracket(s: str) -> str:
-            return NestedCircumfix(("「", "」"), ("『", "』")).fix(s)
-
-        @staticmethod
-        def swap_tabs(s: str) -> str:
-            lines = s.splitlines()
-            if len(lines) < 1:
-                return s
-            swapped = []
-            for line in lines:
-                ss = line.split("\t")
-                ss.insert(0, ss.pop())
-                swapped.append("\t".join(ss))
-            return "\n".join(swapped)
-
-        @staticmethod
-        def mdtable_from_tsv(s: str) -> str:
-            delim = "\t"
-
-            def _split(s: str) -> list[str]:
-                return s.split(delim)
-
-            def _join(ss: list) -> str:
-                pipe = "|"
-                return pipe + pipe.join(ss) + pipe
-
-            lines = s.splitlines()
-            header = _join(_split(lines[0]))
-            sep = _join([":---:" for _ in lines[0].split(delim)])
-            table = [
-                header,
-                sep,
-            ]
-            for line in lines[1:]:
-                table.append(_join(_split(line)))
-            return "\n".join(table)
-
-    def invoke_comment_remover(symbol: str) -> Callable[[str], str]:
-        def _remover(s: str) -> str:
-            return "\n".join(
-                [line for line in s.splitlines() if not line.strip().startswith(symbol)]
-            )
-
-        return _remover
-
-    keymap.cutsom_clipboard_formatter = {}
-
-    class ClipboardFormatMenu:
-        @staticmethod
-        def set_formatter(binding: dict) -> None:
-            for menu, func in binding.items():
-                keymap.cutsom_clipboard_formatter[menu] = func
-
-        @staticmethod
-        def invoke_replacer(search: str, replace_to: str) -> Callable[[str], str]:
-            reg = re.compile(search)
-
-            def _replacer(s: str) -> str:
-                return reg.sub(replace_to, s)
-
-            return _replacer
-
-        @classmethod
-        def set_replacer(cls, binding: dict) -> None:
-            for menu, args in binding.items():
-                keymap.cutsom_clipboard_formatter[menu] = cls.invoke_replacer(*args)
-
-        @staticmethod
-        def invoke_line_jointer(sep: str) -> Callable[[str], str]:
-            def _jointer(s: str) -> str:
-                return sep.join(s.splitlines())
-
-            return _jointer
-
-        @classmethod
-        def set_line_jointer(cls, binding: dict) -> None:
-            for name, sep in binding.items():
-                menu = f"Join lines with {name}"
-                keymap.cutsom_clipboard_formatter[menu] = cls.invoke_line_jointer(sep)
-
-    ClipboardFormatMenu.set_formatter(
-        {
-            "to codeblock": lambda c: f"```\n{c}\n```\n",
-            "swap tabs": FormatTools.swap_tabs,
-            "trim space on line head": FormatTools.trim_space_on_line_head,
-            "to lowercase": lambda c: c.lower(),
-            "to uppercase": lambda c: c.upper(),
-            "to slack feed subscribe": lambda c: f"/feed subscribe {c}",
-            "to slack feed remove": lambda c: f"/feed remove {c}",
-            "to list": FormatTools.to_list,
-            "to deepl-friendly": FormatTools.to_deepl_friendly,
-            "swap abbreviation around colon": FormatTools.swap_abbreviation,
-            "colon to double-dash": FormatTools.colon_to_doubledash,
-            "insert blank line": FormatTools.insert_blank_line,
-            "remove blank line": FormatTools.skip_blank_line,
-            "fix dumb quotation": FormatTools.fix_dumb_quotation,
-            "fix KANGXI RADICALS": lambda s: s.transrate(
-                str.maketrans(KANGXI_RADICAL_MAPPING | RADICAL_MAPPING)
-            ),
-            "fix paren inside bracket": FormatTools.fix_paren_inside_bracket,
-            "to double bracket": FormatTools.to_double_bracket,
-            "to single bracket": FormatTools.to_single_bracket,
-            "TSV to markdown table": FormatTools.mdtable_from_tsv,
-            "split postalcode and address": FormatTools.split_postalcode,
-            "decode url": FormatTools.decode_url,
-            "encode url": FormatTools.encode_url,
-            "to halfwidth": lambda s: to_half_letter(s, False),
-            "to halfwidth (including symbols)": lambda s: to_half_letter(s, True),
-            "to halfwidth symbols": to_half_symbol,
-            "to halfwidth bracktets": to_half_brackets,
-            "to fullwidth": lambda s: to_full_letter(s, False),
-            "to fullwidth (including symbols)": lambda s: to_full_letter(s, True),
-            "to fullwidth symbols": to_full_symbol,
-            "to fullwidth bracktets": to_full_brackets,
-            "trim honorific": FormatTools.trim_honorific,
-            "fix nested paren": FormatTools.format_nested_paren,
-            "fix nested bracket": FormatTools.format_nested_bracket,
-            "remove whitespaces": remove_whitespace,
-            "remove javascript comment line": invoke_comment_remover("// "),
-            "remove python comment line": invoke_comment_remover("# "),
-        }
-    )
-    ClipboardFormatMenu.set_replacer(
-        {
-            "backslash to slash": (r"\\", "/"),
-            "escape backslash": (r"\\", r"\\\\"),
-            "escape double-quotation": (r"\"", r'\\"'),
-            "remove double-quotation": (r'"', ""),
-            "remove single-quotation": (r"'", ""),
-            "remove linebreak": (r"\r?\n", ""),
-            "to sigle line": (r"\r?\n", ""),
-            "remove whitespaces (including linebreak)": (r"\s", ""),
-            "remove non-digit-char": (r"[^\d]", ""),
-            "remove quotations": (r"[\u0022\u0027]", ""),
-            "remove inside paren": (r"[（\(].+?[）\)]", ""),
-            "fix msword-bullet": (
-                r"[\uF06C\uF0D8\uF0B2\uF09F\u25E6\uF0A7\uF06C]\u0009",
-                "\u30fb",
-            ),
-            "remove msword-bullet": (
-                r"[\uF06C\uF0D8\uF0B2\uF09F\u25E6\uF0A7\uF06C]\u0009",
-                "",
-            ),
-            "to curly-comma (\uff0c)": (r"\u3001", "\uff0c"),
-            "to japanese-comma (\u3001)": (r"\uff0c", "\u3001"),
-            "shorten amazon url": (
-                r"^.+amazon\.co\.jp/.+dp/(.{10}).*",
-                r"https://www.amazon.jp/dp/\1",
-            ),
-        }
-    )
-
-    ClipboardFormatMenu.set_line_jointer(
-        {
-            "Half-Comma": ",",
-            "Dot": "・",
-            "Tab": "\t",
-            "Slash": "／",
-            "Pipe": "|",
-        }
-    )
-
     def fzfmenu() -> None:
         if not check_fzf():
             balloon(keymap, "cannot find fzf on PC.")
@@ -1070,7 +754,7 @@ def setup(keymap) -> None:
             balloon(keymap, "no text in clipboard.")
             return
 
-        table = keymap.cutsom_clipboard_formatter
+        table = CLIPBOARD_FORMATTER_MAPPING
 
         def _fzf(job_item: ckit.JobItem) -> None:
             job_item.func = None
@@ -1085,10 +769,10 @@ def setup(keymap) -> None:
             )
             try:
                 if proc.stdin:
-                    for k in table.keys():
+                    for k in table:
                         proc.stdin.write(k + "\n")
                     proc.stdin.close()
-            except Exception as e:
+            except Exception as e:  # noqa: BLE001
                 balloon(keymap, e)
                 return
 
--- Keyhac/config/tools/format_str.py
+++ Keyhac/config/tools/format_str.py
@@ -1,3 +1,10 @@
+import re
+import urllib.parse
+from collections.abc import Callable
+
+from .char_width import to_half_letter
+
+
 def remove_whitespace(s: str) -> str:
     return s.strip().translate(
         str.maketrans(
@@ -13,16 +20,184 @@ def simple_quote(s: str) -> str:
     return "\n".join([">" + line for line in lines])
 
 
-def as_single_line(s: str) -> str:
+def as_single_quoted_line(s: str) -> str:
     lines = s.strip().splitlines()
     return ">" + "".join([line.strip() for line in lines])
 
 
+def invoke_comment_remover(symbol: str) -> Callable[[str], str]:
+    def _remover(s: str) -> str:
+        return "\n".join(
+            [line for line in s.splitlines() if not line.strip().startswith(symbol)]
+        )
+
+    return _remover
+
+
+class NestedCircumfix:
+    def __init__(self, prime_pair: tuple, secondary_pair: tuple):
+        self.pairs = [prime_pair, secondary_pair]
+
+    def fix(self, s: str) -> str:
+        stack = []
+        result = list(s)
+        openChar, closeChar = self.pairs[0]
+        for i, char in enumerate(s):
+            if char == openChar:
+                stack.append(i)
+            else:
+                if char == closeChar and stack:
+                    start = stack.pop()
+                    depth = len(stack)
+                    left, right = self.pairs[depth % 2]
+                    result[start] = left
+                    result[i] = right
+
+        return "".join(result)
+
+
+def swap_abbreviation(s: str) -> str:
+    ss = re.split(r"[:：]\s*", s)
+    if len(ss) == 2:
+        return ss[1] + "：" + ss[0]
+    return ""
+
+
+def colon_to_doubledash(s: str) -> str:
+    return re.sub(r"[:：]\s*", "\u2015\u2015", s)
+
+
 def skip_blank_line(s: str) -> str:
+    lines = s.strip().splitlines()
+    return "\n".join([line for line in lines if line.strip()])
+
+
+def insert_blank_line(s: str) -> str:
     lines = []
     for line in s.strip().splitlines():
-        if 0 < len(line.strip()):
-            lines.append(">" + line)
-        else:
-            lines.append("")
+        lines.append(line.strip())
+        lines.append("")
     return "\n".join(lines)
+
+
+def to_double_bracket(s: str) -> str:
+    reg = re.compile(r"[\u300c\u300d]")
+
+    def _replacer(mo: re.Match) -> str:
+        if mo.group(0) == "\u300c":
+            return "\u300e"
+        return "\u300f"
+
+    return reg.sub(_replacer, s)
+
+
+def to_single_bracket(s: str) -> str:
+    reg = re.compile(r"[\u300e\u300f]")
+
+    def _replacer(mo: re.Match) -> str:
+        if mo.group(0) == "\u300e":
+            return "\u300c"
+        return "\u300d"
+
+    return reg.sub(_replacer, s)
+
+
+def to_list(s: str) -> str:
+    lines = s.splitlines()
+    return "\n".join(["- " + line for line in lines])
+
+
+def split_postalcode(s: str) -> str:
+    lines = s.splitlines()
+    if 1 < len(lines):
+        reg = re.compile(r"(\d{3}).(\d{4})[ 　]*(.+$)")
+    else:
+        reg = re.compile(r"(\d{3}).(\d{4})[\s]*(.+$)")
+    ss = []
+    for line in lines:
+        hankaku = to_half_letter(line.strip().strip("\u3012"), True)
+        m = reg.match(hankaku)
+        if m:
+            ss.append(f"{m.group(1)}-{m.group(2)}\t{m.group(3)}")
+        else:
+            ss.append(line)
+    return "\n".join(ss)
+
+
+def fix_paren_inside_bracket(s: str) -> str:
+    reg = re.compile(r"(\(.+?\)|（.+?）)」")
+
+    def _replacer(mo: re.Match) -> str:
+        return "」" + mo.group(1)
+
+    return reg.sub(_replacer, s)
+
+
+def fix_dumb_quotation(s: str) -> str:
+    reg = re.compile(r"\"([^\"]+?)\"|'([^']+?)'")
+
+    def _replacer(mo: re.Match) -> str:
+        if str(mo.group(0)).startswith('"'):
+            return f"\u201c{mo.group(1)}\u201d"
+        return f"\u2018{mo.group(1)}\u2019"
+
+    return reg.sub(_replacer, s)
+
+
+def decode_url(s: str) -> str:
+    return urllib.parse.unquote(s)
+
+
+def encode_url(s: str) -> str:
+    return urllib.parse.quote(s)
+
+
+def trim_honorific(s: str) -> str:
+    reg = re.compile(r"先生$|様$|(先生|様)(?=[、。：；（）［］・！？\s])")
+    return reg.sub("", s)
+
+
+def trim_space_on_line_head(s: str) -> str:
+    return "\n".join([line.lstrip() for line in s.splitlines()])
+
+
+def format_nested_paren(s: str) -> str:
+    return NestedCircumfix(("（", "）"), ("〔", "〕")).fix(s)
+
+
+def format_nested_bracket(s: str) -> str:
+    return NestedCircumfix(("「", "」"), ("『", "』")).fix(s)
+
+
+def swap_tabs(s: str) -> str:
+    lines = s.splitlines()
+    if len(lines) < 1:
+        return s
+    swapped = []
+    for line in lines:
+        ss = line.split("\t")
+        ss.insert(0, ss.pop())
+        swapped.append("\t".join(ss))
+    return "\n".join(swapped)
+
+
+def mdtable_from_tsv(s: str) -> str:
+    delim = "\t"
+
+    def _split(s: str) -> list[str]:
+        return s.split(delim)
+
+    def _join(ss: list) -> str:
+        pipe = "|"
+        return pipe + pipe.join(ss) + pipe
+
+    lines = s.splitlines()
+    header = _join(_split(lines[0]))
+    sep = _join([":---:" for _ in lines[0].split(delim)])
+    table = [
+        header,
+        sep,
+    ]
+    for line in lines[1:]:
+        table.append(_join(_split(line)))
+    return "\n".join(table)
