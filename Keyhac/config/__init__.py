06b1420a1f720f91ba8853f330e89cf788624e6e
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-03 14:49:59 +0900

    split app based bindings

--- Keyhac/config/__init__.py
+++ Keyhac/config/__init__.py
@@ -1,6 +1,7 @@
-from . import main, style  # noqa: N999
+from . import app_based, main, style  # noqa: N999
 
 
 def configure(keymap) -> None:
     style.setup(keymap)
     main.setup(keymap)
+    app_based.setup(keymap)
