8784f2388eb467117e19245e09cfe22b668090c6
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-04 10:39:29 +0900

    split general setting

--- Keyhac/config/__init__.py
+++ Keyhac/config/__init__.py
@@ -2,6 +2,24 @@ from . import app_specific, main, style  # noqa: N999
 
 
 def configure(keymap) -> None:
+
+    # user modifier
+    keymap.replaceKey("(29)", 235)  # "muhenkan" => 235
+    keymap.replaceKey("(28)", 236)  # "henkan" => 236
+    keymap.defineModifier(235, "User0")  # "muhenkan" => "U0"
+    keymap.defineModifier(236, "User1")  # "henkan" => "U1"
+
+    # enable clipbard history
+    keymap.clipboard_history.enableHook(True)
+
+    # history max size
+    keymap.clipboard_history.maxnum = 200
+    keymap.clipboard_history.quota = 10 * 1024 * 1024
+
+    # quote mark when paste with Ctrl.
+    keymap.quote_mark = "> "
+
+    # load settings
     style.setup(keymap)
     main.setup(keymap)
     app_specific.setup(keymap)
--- Keyhac/config/main.py
+++ Keyhac/config/main.py
@@ -55,26 +55,6 @@ def setup(keymap) -> None:
     window_activate_tool.setup(keymap)
 
     ################################
-    # general setting
-    ################################
-
-    # user modifier
-    keymap.replaceKey("(29)", 235)  # "muhenkan" => 235
-    keymap.replaceKey("(28)", 236)  # "henkan" => 236
-    keymap.defineModifier(235, "User0")  # "muhenkan" => "U0"
-    keymap.defineModifier(236, "User1")  # "henkan" => "U1"
-
-    # enable clipbard history
-    keymap.clipboard_history.enableHook(True)
-
-    # history max size
-    keymap.clipboard_history.maxnum = 200
-    keymap.clipboard_history.quota = 10 * 1024 * 1024
-
-    # quote mark when paste with Ctrl.
-    keymap.quote_mark = "> "
-
-    ################################
     # key remap
     ################################
 
