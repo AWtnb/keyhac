8b007bb68524ded1a12246f3304a9b9c93bc7ffe
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-05 11:08:12 +0900

    chore

--- Keyhac/config/tools/format_clipboard.py
+++ Keyhac/config/tools/format_clipboard.py
@@ -36,52 +36,46 @@ from .str_tools import (
 CLIPBOARD_FORMATTER_MAPPING = {}
 
 
-def set_formatter(binding: dict) -> None:
-    for menu, func in binding.items():
-        CLIPBOARD_FORMATTER_MAPPING[menu] = func
-
-
-set_formatter(
-    {
-        "to codeblock": lambda c: f"```\n{c}\n```\n",
-        "swap tabs": swap_tabs,
-        "trim space on line head": trim_space_on_line_head,
-        "to lowercase": lambda c: c.lower(),
-        "to uppercase": lambda c: c.upper(),
-        "to slack feed subscribe": lambda c: f"/feed subscribe {c}",
-        "to slack feed remove": lambda c: f"/feed remove {c}",
-        "to list": to_list,
-        "swap abbreviation around colon": swap_abbreviation,
-        "colon to double-dash": colon_to_doubledash,
-        "insert blank line": insert_blank_line,
-        "remove blank line": skip_blank_line,
-        "fix dumb quotation": fix_dumb_quotation,
-        "fix KANGXI RADICALS": lambda s: s.transrate(
-            str.maketrans(KANGXI_RADICAL_MAPPING | RADICAL_MAPPING)
-        ),
-        "fix paren inside bracket": fix_paren_inside_bracket,
-        "to double bracket": to_double_bracket,
-        "to single bracket": to_single_bracket,
-        "TSV to markdown table": mdtable_from_tsv,
-        "split postalcode and address": split_postalcode,
-        "decode url": decode_url,
-        "encode url": encode_url,
-        "to halfwidth": lambda s: to_half_letter(s, False),
-        "to halfwidth (including symbols)": lambda s: to_half_letter(s, True),
-        "to halfwidth symbols": to_half_symbol,
-        "to halfwidth bracktets": to_half_brackets,
-        "to fullwidth": lambda s: to_full_letter(s, False),
-        "to fullwidth (including symbols)": lambda s: to_full_letter(s, True),
-        "to fullwidth symbols": to_full_symbol,
-        "to fullwidth bracktets": to_full_brackets,
-        "trim honorific": trim_honorific,
-        "fix nested paren": format_nested_paren,
-        "fix nested bracket": format_nested_bracket,
-        "remove whitespaces": remove_whitespace,
-        "remove javascript comment line": invoke_comment_remover("// "),
-        "remove python comment line": invoke_comment_remover("# "),
-    }
-)
+for menu, func in {
+    "to codeblock": lambda c: f"```\n{c.rstrip()}\n```\n\n",
+    "swap tabs": swap_tabs,
+    "trim space on line head": trim_space_on_line_head,
+    "to lowercase": lambda c: c.lower(),
+    "to uppercase": lambda c: c.upper(),
+    "to slack feed subscribe": lambda c: f"/feed subscribe {c}",
+    "to slack feed remove": lambda c: f"/feed remove {c}",
+    "to list": to_list,
+    "swap abbreviation around colon": swap_abbreviation,
+    "colon to double-dash": colon_to_doubledash,
+    "insert blank line": insert_blank_line,
+    "remove blank line": skip_blank_line,
+    "fix dumb quotation": fix_dumb_quotation,
+    "fix KANGXI RADICALS": lambda s: s.transrate(
+        str.maketrans(KANGXI_RADICAL_MAPPING | RADICAL_MAPPING)
+    ),
+    "fix paren inside bracket": fix_paren_inside_bracket,
+    "to double bracket": to_double_bracket,
+    "to single bracket": to_single_bracket,
+    "TSV to markdown table": mdtable_from_tsv,
+    "split postalcode and address": split_postalcode,
+    "decode url": decode_url,
+    "encode url": encode_url,
+    "to halfwidth": lambda s: to_half_letter(s, False),
+    "to halfwidth (including symbols)": lambda s: to_half_letter(s, True),
+    "to halfwidth symbols": to_half_symbol,
+    "to halfwidth bracktets": to_half_brackets,
+    "to fullwidth": lambda s: to_full_letter(s, False),
+    "to fullwidth (including symbols)": lambda s: to_full_letter(s, True),
+    "to fullwidth symbols": to_full_symbol,
+    "to fullwidth bracktets": to_full_brackets,
+    "trim honorific": trim_honorific,
+    "fix nested paren": format_nested_paren,
+    "fix nested bracket": format_nested_bracket,
+    "remove whitespaces": remove_whitespace,
+    "remove javascript comment line": invoke_comment_remover("// "),
+    "remove python comment line": invoke_comment_remover("# "),
+}.items():
+    CLIPBOARD_FORMATTER_MAPPING[menu] = func
 
 
 def invoke_replacer(search: str, replace_to: str) -> Callable[[str], str]:
@@ -93,40 +87,34 @@ def invoke_replacer(search: str, replace_to: str) -> Callable[[str], str]:
     return _replacer
 
 
-def set_replacer(binding: dict) -> None:
-    for menu, args in binding.items():
-        CLIPBOARD_FORMATTER_MAPPING[menu] = invoke_replacer(*args)
-
-
-set_replacer(
-    {
-        "backslash to slash": (r"\\", "/"),
-        "escape backslash": (r"\\", r"\\\\"),
-        "escape double-quotation": (r"\"", r'\\"'),
-        "remove double-quotation": (r'"', ""),
-        "remove single-quotation": (r"'", ""),
-        "remove linebreak": (r"\r?\n", ""),
-        "to sigle line": (r"\r?\n", ""),
-        "remove whitespaces (including linebreak)": (r"\s", ""),
-        "remove non-digit-char": (r"[^\d]", ""),
-        "remove quotations": (r"[\u0022\u0027]", ""),
-        "remove inside paren": (r"[（\(].+?[）\)]", ""),
-        "fix msword-bullet": (
-            r"[\uF06C\uF0D8\uF0B2\uF09F\u25E6\uF0A7\uF06C]\u0009",
-            "\u30fb",
-        ),
-        "remove msword-bullet": (
-            r"[\uF06C\uF0D8\uF0B2\uF09F\u25E6\uF0A7\uF06C]\u0009",
-            "",
-        ),
-        "to curly-comma (\uff0c)": (r"\u3001", "\uff0c"),
-        "to japanese-comma (\u3001)": (r"\uff0c", "\u3001"),
-        "shorten amazon url": (
-            r"^.+amazon\.co\.jp/.+dp/(.{10}).*",
-            r"https://www.amazon.jp/dp/\1",
-        ),
-    }
-)
+for menu, args in {
+    "backslash to slash": (r"\\", "/"),
+    "escape backslash": (r"\\", r"\\\\"),
+    "escape double-quotation": (r"\"", r'\\"'),
+    "remove double-quotation": (r'"', ""),
+    "remove single-quotation": (r"'", ""),
+    "remove linebreak": (r"\r?\n", ""),
+    "to sigle line": (r"\r?\n", ""),
+    "remove whitespaces (including linebreak)": (r"\s", ""),
+    "remove non-digit-char": (r"[^\d]", ""),
+    "remove quotations": (r"[\u0022\u0027]", ""),
+    "remove inside paren": (r"[（\(].+?[）\)]", ""),
+    "fix msword-bullet": (
+        r"[\uF06C\uF0D8\uF0B2\uF09F\u25E6\uF0A7\uF06C]\u0009",
+        "\u30fb",
+    ),
+    "remove msword-bullet": (
+        r"[\uF06C\uF0D8\uF0B2\uF09F\u25E6\uF0A7\uF06C]\u0009",
+        "",
+    ),
+    "to curly-comma (\uff0c)": (r"\u3001", "\uff0c"),
+    "to japanese-comma (\u3001)": (r"\uff0c", "\u3001"),
+    "shorten amazon url": (
+        r"^.+amazon\.co\.jp/.+dp/(.{10}).*",
+        r"https://www.amazon.jp/dp/\1",
+    ),
+}.items():
+    CLIPBOARD_FORMATTER_MAPPING[menu] = invoke_replacer(*args)
 
 
 def invoke_line_jointer(sep: str) -> Callable[[str], str]:
@@ -136,18 +124,12 @@ def invoke_line_jointer(sep: str) -> Callable[[str], str]:
     return _jointer
 
 
-def set_line_jointer(binding: dict) -> None:
-    for name, sep in binding.items():
-        menu = f"Join lines with {name}"
-        CLIPBOARD_FORMATTER_MAPPING[menu] = invoke_line_jointer(sep)
-
-
-set_line_jointer(
-    {
-        "Half-Comma": ",",
-        "Dot": "・",
-        "Tab": "\t",
-        "Slash": "／",
-        "Pipe": "|",
-    }
-)
+for name, sep in {
+    "Half-Comma": ",",
+    "Dot": "・",
+    "Tab": "\t",
+    "Slash": "／",
+    "Pipe": "|",
+}.items():
+    menu = f"Join lines with {name}"
+    CLIPBOARD_FORMATTER_MAPPING[menu] = invoke_line_jointer(sep)
