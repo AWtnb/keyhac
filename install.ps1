b68d2458bbca8358f7e65a97ae9cea45b084ada5
awtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-16 13:17:55 +0900

    fix

--- install.ps1
+++ install.ps1
@@ -2,4 +2,4 @@ $d = "Keyhac"
 $dataPath = $env:APPDATA | Join-Path -ChildPath $d
 Remove-Item -Path $dataPath -Force -Recurse -ErrorAction SilentlyContinue > $null
 $srcPath = $PSScriptRoot | Join-Path -ChildPath $d
-New-Item -Path $dataPath -Value $srcPath -ItemType Junction -Confirm -Force
+New-Item -Path $dataPath -Value $srcPath -ItemType Junction -Force
