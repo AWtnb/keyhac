40719d0b6665cea7b827bdce47e93fbf0cae23b3
awtnb <46807238+AWtnb@users.noreply.github.com>
2026-09-16 23:58:12 +0900

    update

--- .keyhac/config.py
+++ .keyhac/config.py
@@ -4,13 +4,18 @@ import sys
 from types import ModuleType
 
 
-def import_config(config_module_name: str) -> ModuleType:
+def register_config_path(dir_name: str) -> None:
     user_profile = os.environ.get("USERPROFILE")
     assert user_profile is not None
-    config_dir = os.path.join(user_profile, ".keyhac")
+    config_dir = os.path.join(user_profile, dir_name)
     if config_dir not in sys.path:
         sys.path.insert(0, config_dir)
 
+
+register_config_path(".keyhac")
+
+
+def import_config(config_module_name: str) -> ModuleType:
     for name in list(sys.modules):
         if name == config_module_name or name.startswith(config_module_name + "."):
             del sys.modules[name]
